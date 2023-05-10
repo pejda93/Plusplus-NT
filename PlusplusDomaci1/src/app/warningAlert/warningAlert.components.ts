e915acce8c61090f1e6ed3e837f7d9734e7928c8
pejda93 <bladeofweed93@yahoo.com>
2023-05-10 11:41:28 +0200

    Prvi domaci

---
+++ PlusplusDomaci1/src/app/warningAlert/warningAlert.components.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-warning-alert' ,
+  templateUrl: 'warningAlert.components.html',
+  styleUrls: ['warningAlert.components.css']
+
+})
+export class WarningAlertComponents{
+
+}
